4b1186a2b07a803de3b4c23643b0cd7056c020ab
alissa borg <alissaborg@alissas-MacBook-Air.local>
2024-07-20 19:39:37 +1000

    Added function to calculate sum of two numbers and updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+// Existing code
+console.log("Hello World");
+
+// New function to add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Print the sum of 5 and 10
+console.log("The sum of 5 and 10 is:", addNumbers(5, 10));
